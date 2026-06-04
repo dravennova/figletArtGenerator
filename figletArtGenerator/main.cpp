893131a8bc8e23593be47f31c2aa9c988e5855cc
Draven Nova <draven77medina@hotmail.com>
2026-06-04 09:30:02 -0700

    Initial Commit

---
+++ figletArtGenerator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  figletArtGenerator
+//
+//  Created by Draven Nova on 6/4/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
